58d54a9143dad2a2cf0ea96372fec0cb5e84adb2
Aleksi Kinnunen <aleksi@makinen.local>
2026-03-15 14:05:33 +0200

    change cpus-per-task config

--- batch_scripts/test_scripts/run_test_models.sh
+++ batch_scripts/test_scripts/run_test_models.sh
@@ -6,7 +6,9 @@
 #SBATCH --account=project_462001312
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --partition=standard-g
+#SBATCH --cpus-per-task=128
 
 module load cray-python
 
