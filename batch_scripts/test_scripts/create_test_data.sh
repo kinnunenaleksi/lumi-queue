7dc6b26f0c662c3c414c843048c0c7bddb5d9078
Aleksi Kinnunen <aleksi@makinen.local>
2026-03-15 11:41:22 +0200

    lumi params for .sh

--- batch_scripts/test_scripts/create_test_data.sh
+++ batch_scripts/test_scripts/create_test_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=testRunModels
+#SBATCH --job-name=testCreateDatasets
 #SBATCH --output=runs/r-%x.%j.out
 #SBATCH --error=runs/r-%x.%j.err
 #SBATCH --account=project_462001312
-#SBATCH --time=00:02:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=16G
-#SBATCH --partition=small-g
+#SBATCH --partition=small
 
 module load cray-python
 
@@ -19,5 +19,5 @@ $HOME/.local/bin/uv sync
 
 $HOME/.local/bin/uv pip install -e .
 
-srun python batch_scripts/scripts/tests/test_pipeline.py
+srun python batch_scripts/scripts/tests/create_test_data.py
 
---
+++ batch_scripts/test_scripts/run_test_models.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=testRunModels
+#SBATCH --output=runs/r-%x.%j.out
+#SBATCH --error=runs/r-%x.%j.err
+#SBATCH --account=project_462001312
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=small-g
+
+module load cray-python
+
+source .venv/bin/activate
+
+$HOME/.local/bin/uv sync
+
+$HOME/.local/bin/uv pip install -e .
+
+srun python batch_scripts/scripts/tests/run_test_models.py
