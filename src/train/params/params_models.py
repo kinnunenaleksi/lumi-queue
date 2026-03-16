c3db200683488a23058f4373fa7812799625a8dd
Aleksi Kinnunen <aleksi@makinen.local>
2026-03-16 16:22:13 +0200

    change scoring to mae

--- src/train/params/params_models.py
+++ src/train/params/params_models.py
@@ -22,8 +22,8 @@ class ModelConfig:
     grid_search_kwargs: dict[str, Any] = field(
         default_factory=lambda: {
             # "scoring": "neg_mean_squared_error",
-            # "scoring": "neg_mean_absolut_error",
-            "scoring": "r2",
+            "scoring": "neg_mean_absolute_error",
+            # "scoring": "r2",
             "refit": True,
             "n_jobs": -1,
         }
