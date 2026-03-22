87e0983920d565b6aac19d84819ad880a17787d6
Aleksi Kinnunen <aleksi@makinen.local>
2026-03-22 15:47:55 +0200

    split-method to random

--- batch_scripts/test_scripts/run_test_models.py
+++ batch_scripts/test_scripts/run_test_models.py
@@ -52,7 +52,8 @@ def main():
         # y_col="wait_time_seconds",
         y_col="wait_time_minutes",
         test_size=0.2,
-        split_method="timeseries",
+        # split_method="timeseries",
+        split_method="random",
         export_path=EXPORT_PATH,
         input_path=INPUT_PATH,
     )
