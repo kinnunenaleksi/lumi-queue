d34c25433ec65fb3f5d85be4bf52f9efea3b9fc4
Aleksi Kinnunen <aleksi@makinen.local>
2026-03-25 16:56:59 +0200

    more training time

--- batch_scripts/test_ablation.sh
+++ batch_scripts/test_ablation.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=runs/r-%x.%j.out
 #SBATCH --error=runs/r-%x.%j.err
 #SBATCH --account=project_462001312
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=standard
